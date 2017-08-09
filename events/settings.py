28c4dfd02b33b4d6a13efea0253cff5e3d25f5ce
Egorov Evgenii Gennadevich <egorov_evgeny@bk.ru>
2017-08-09 17:19:00 +0300

    db deployment

--- events/settings.py
+++ events/settings.py
@@ -130,28 +130,23 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = 'app/static'
 
-PROJECT_DOMAIN = 'localhost'
-PROJECT_PORT = '8000'
+PROJECT_DOMAIN = 'evening-ocean-72138.herokuapp.com'
+PROJECT_PORT = '80'
 
 # Настройки Heroku
-if os.getcwd() == '/app':
-    import dj_database_url
-    DATABASES = {
-        'default': dj_database_url.config(default='postgres://localhost')
-    }
-    # Поддержка заголовка 'X-Forwarded-Proto' для request.is_secure().
-    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-    # Разрешены все заголовки хостов.
-    ALLOWED_HOSTS = ['*']
+import dj_database_url
 
-    # Конфигурация статических ресурсов
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = 'app/static'
-    STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'static'),
-    )
-else:
-    try:
-        from .local_settings import *
-    except ImportError as e:
-        raise Exception('Unable to find local settings') from e
+DATABASES = {
+    'default': dj_database_url.config(default='postgres://localhost')
+}
+# Поддержка заголовка 'X-Forwarded-Proto' для request.is_secure().
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# Разрешены все заголовки хостов.
+ALLOWED_HOSTS = ['*']
+
+# Конфигурация статических ресурсов
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
